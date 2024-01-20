ee39e16f18dd1b91debbb0866c491655377183b5
Paul Nivoix <fozkyz@gmail.com>
2024-01-20 10:28:30 +0100

    Setup project

---
+++ Charcoal.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Charcoal;
+
+namespace Charcoal.Test
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			using (var game = new Charcoal.Game1())
+			{
+				game.Run();
+			}
+		}
+	}
+}
