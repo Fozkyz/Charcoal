6168335a237d5d93bf32d9e9d37ab970c72f62bd
Paul Nivoix <fozkyz@gmail.com>
2024-01-21 10:25:16 +0100

    Added Gameobject, Component, Transform and Scene classes

---
+++ Charcoal/Components/Transform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charcoal.Components
+{
+	public class Transform : Component
+	{
+
+		public GameObject GameObject { get; set; }
+
+		public List<Transform> Transforms = new List<Transform>();
+
+		public Transform Root { get; set; }
+
+		public Transform Parent { get; set; }
+
+		public void Awake()
+		{
+
+		}
+	}
+}
