6d873f7b2d1b3c853fcb2d7cfba7ed87acc2a41c
Paul Nivoix <fozkyz@gmail.com>
2024-01-21 08:35:23 +0100

    Moved application files to new Application folder

---
+++ Charcoal/Application/Application.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charcoal.Application
+{
+    public class Application
+    {
+        public static Engine Engine { get; set; }
+
+        public static ContentManager Content { get; set; }
+
+        public static GraphicsDevice GraphicsDevice { get; set; }
+
+        public static GraphicsDeviceManager GraphicsDeviceManager { get; set; }
+
+        public static void Quit()
+        {
+            Engine.Exit();
+        }
+    }
+}
